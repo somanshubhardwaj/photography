bd6359614a876143854a20488a41b259e35f45b7
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-03-23 21:03:02 +0530

    Update image layout and styling in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,7 +20,24 @@ const Main = () => {
           <img src={img4} alt="" />
           <img src={img5} alt="" />
           <img src={img6} alt="" />
+          <img src={img5} alt="" />
           <img src={img1} alt="" />
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+          <img src={img4} alt="" />
+          <img src={img5} alt="" />
+          <img src={img5} alt="" />
+          <img src={img6} alt="" />
+          <img src={img1} alt="" />
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+          <img src={img4} alt="" />
+          <img src={img5} alt="" />
+          <img src={img6} alt="" />
+          <img src={img1} alt="" />
+          <img src={img5} alt="" />
         </div>
       </div>
     </div>
